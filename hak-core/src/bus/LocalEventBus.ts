ec055d8bbc4fac06a4ada533cc1071ff00f9fc79
Doug Lampe <douglampe@users.noreply.github.com>
2022-02-18 17:38:00 -0500

    feat: add EventBus and implement in LocalEventBus (#2)
    * feat: add EventBus and implement in LocalEventBus
    
    * chore: add build workflow

---
+++ hak-core/src/bus/LocalEventBus.ts
@@ -0,0 +1,34 @@
+import { EventEmitter } from 'events';
+import { EventBus } from './EventBus';
+
+/**
+ * Provides an implementation of EventBus for local in-memory event handling.
+ */
+export class LocalEventBus implements EventBus {
+  private eventEmitter: EventEmitter;
+
+  /**
+   * Creates a new instance of LocalEventBus.
+   */
+  constructor() {
+    this.eventEmitter = new EventEmitter();
+  }
+
+  /**
+   * Adds a listener for events with the specified name.
+   * @param event Name of event to listen for
+   * @param callback Callback to execute when an event is fired with the specified name
+   */
+  public addListener<T>(event: string, callback: (payload: T) => void) {
+    this.eventEmitter.on(event, callback);
+  }
+
+  /**
+   * Fires an event with the specified name.
+   * @param event Name of event to fire
+   * @param payload Contents of event to pass to callback of registered listeners
+   */
+  public fireEvent<T>(event: string, payload: T) {
+    this.eventEmitter.emit(event, payload);
+  }
+}
